45c7cf1958009f1c334e9a6fc405a1a39864df36
Yu-Fang-Hong <41023109@gm.nfu.edu.tw>
2023-03-08 20:05:27 +0800

    功課w3

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '組員:洪于芳、李宛妮 \n 倉儲: https://github.com/mdecd2023/2a-pj1ag20 \n 網頁: https://mdecd2023.github.io/2a-pj1ag20/ \n', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '組員:洪于芳、李宛妮 \n 倉儲: https://github.com/mdecd2023/2a-pj1ag20 \n 網頁: https://mdecd2023.github.io/2a-pj1ag20/ \n', 'tags': '', 'url': 'About.html'}, {'title': 'homework', 'text': '', 'tags': '', 'url': 'homework.html'}, {'title': 'w3', 'text': '', 'tags': '', 'url': 'w3.html'}]};
